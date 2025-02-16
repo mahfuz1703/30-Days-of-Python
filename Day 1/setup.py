1e6c09e43f941c0c641f2839c5413768e771a5bd
Mahfuz Mia <sayhello.mahfuz@gmail.com>
2025-02-16 16:25:35 +0600

    Day 1: Setup python environment

---
+++ Day 1/setup.py
@@ -0,0 +1,7 @@
+# Install python form here: https://www.python.org/downloads/
+
+# Install VS Code or any other IDE of your choice
+
+# Install VS Code from here: https://code.visualstudio.com/download
+# Install Python extension for VS Code
+# Install Python extension for VS Code from here: https://marketplace.visualstudio.com/items?itemName=ms-python.python
